ad1c5ac5fedf0a5ed8612d1cfbe6d087c59768e4
joseaam2020 <joseaam2019@hotmail.com>
2020-09-25 10:19:08 -0600

    Se elimina el uso de Main. Se agregan opciones para añadir IP y puerto de destino. Se agrega la habilidad para conectarse a un puerto de manera automatica y se muestra el puerto encontrado en el programa. Tambien se añade color a la interfaz

--- Main.java
+++
@@ -1,8 +0,0 @@
-package tarea.tareaChat;
-
-public class Main {
-    public static void main(String[] args) {
-        User.main(new int[]{4444,5555});
-        User.main(new int[]{5555,4444});
-    }
-}
--- User.java
+++ User.java
@@ -10,16 +10,20 @@ import java.io.IOException;
 import java.net.*;
 
 public class User {
-    public static void main(int[] args) {
-        UserFrame newFrame = new UserFrame(args);
+    public static void main(String[] args) {
+        UserFrame newFrame = new UserFrame("Chat");
         newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
 
 class UserFrame extends JFrame{
-    public UserFrame(int[] ports){
+    public UserFrame(String title){
+        super(title);
+        ImageIcon icon = new ImageIcon("C:\\Users\\Z420\\" +
+                "IdeaProjects\\TareaChat\\src\\tarea\\tareaChat\\chat-bubble.png");
+        this.setIconImage(icon.getImage());
         setBounds(600,300,280,350);
-        UserPanel newUser = new UserPanel(ports);
+        UserPanel newUser = new UserPanel();
         add(newUser);
         setVisible(true);
     }
@@ -29,49 +33,60 @@ class UserPanel extends JPanel implements Runnable{
     private JTextField campoTexto;
     private JTextField campoNombre;
     private JTextField ipAddress;
+    private JTextField campoOut;
     private JButton boton1;
     private JTextArea campoMensajes;
+    private JLabel userPort;
     private JScrollPane marcoMensajes;
-    private int inputPort;
-    private int outputPort;
 
-    public UserPanel(int[] ports){
-        inputPort = ports[0];
-        outputPort = ports [1];
+    public UserPanel(){
+        Color panelBackground = Color.decode("#292F36");
+        Color labelColor = Color.decode("#FFFFFF");
+        Color portColor = Color.decode("#FF6B6B");
+        Color botonColor = Color.decode("#4ECDC4");
+        this.setBackground(panelBackground);
         JLabel nombre = new JLabel("Nombre:");
+        nombre.setForeground(labelColor);
         add(nombre);
         campoNombre = new JTextField(20);
         add(campoNombre);
-        JLabel ip = new JLabel("Ip de destinatario:");
+        JLabel ip = new JLabel("Ip del destinatario:");
+        ip.setForeground(labelColor);
         add(ip);
         ipAddress = new JTextField(10);
         add(ipAddress);
-        campoMensajes = new JTextArea(12,26);
+        JLabel out = new JLabel("Puerto del destinatario:");
+        out.setForeground(labelColor);
+        add(out);
+        campoOut = new JTextField(10);
+        campoOut.setText("0");
+        add(campoOut);
+        campoMensajes = new JTextArea(10,26);
         campoMensajes.setEditable(false);
         marcoMensajes = new JScrollPane();
         marcoMensajes.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-        Color marco = new Color(0,102,255);
-        marcoMensajes.getViewport().setBackground(marco);
         marcoMensajes.getViewport().add(campoMensajes);
         add(marcoMensajes);
         campoTexto = new JTextField(20);
         add(campoTexto);
         boton1 = new JButton("Enviar");
-        TextoBoton newTextoBoton = new TextoBoton(this.outputPort,ipAddress.getText());
+        TextoBoton newTextoBoton = new TextoBoton();
         boton1.addActionListener(newTextoBoton);
+        boton1.setBackground(botonColor);
+        boton1.setForeground(labelColor);
         add(boton1);
         Thread userThread = new Thread(this);
         userThread.start();
+        userPort = new JLabel("");
+        userPort.setForeground(portColor);
+        add(userPort);
+        campoOut.setText("");
     }
 
-    private class TextoBoton implements ActionListener{
+    private class TextoBoton implements ActionListener{ ;
 
-        private int outputPort;
-        private String ip;
+        public TextoBoton(){
 
-        public TextoBoton(int newOutport,String ip){
-            this.outputPort = newOutport;
-            this.ip = ip;
         }
 
         public void actionPerformed(ActionEvent e) {
@@ -79,20 +94,27 @@ class UserPanel extends JPanel implements Runnable{
             campoMensajes.append(String.valueOf(texto)+"\n");
             campoTexto.setText("");
             try {
-                Socket newSocket = new Socket(this.ip,this.outputPort);
+                int port = Integer.parseInt(campoOut.getText());
+                Socket newSocket = new Socket(ipAddress.getText(),port);
                 DataOutputStream output = new DataOutputStream(newSocket.getOutputStream());
                 output.writeUTF(String.valueOf(texto));
                 output.close();
             } catch (IOException ioException) {
-                System.out.println("Here");
+                System.out.println("Sending");
                 System.out.println(ioException.getMessage());;
+            } catch (NumberFormatException numberFormatException){
+                System.out.println("Port error");
+                System.out.println(numberFormatException.getMessage());
             }
         }
     }
 
     public void run() {
         try {
-            ServerSocket newSS = new ServerSocket(this.inputPort);
+            ServerSocket newSS = new ServerSocket(0);
+            StringBuilder port = new StringBuilder();
+            port.append(String.valueOf(newSS.getLocalPort()));
+            userPort.setText("Utilizando puerto:" + String.valueOf(port));
             while(true){
                 StringBuilder texto = new StringBuilder();
                 Socket inputSocket = newSS.accept();
@@ -106,5 +128,4 @@ class UserPanel extends JPanel implements Runnable{
             e.printStackTrace();
         }
     }
-
 }
