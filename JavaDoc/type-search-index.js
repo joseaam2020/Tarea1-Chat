d8e36bf34d58a368e20eac8f99008109ce7f10aa
joseaam2020 <joseaam2019@hotmail.com>
2020-09-25 19:09:57 -0600

    Se agregan los documentos generados por JavaDoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"tarea.tareaChat","l":"UserPanel.TextoBoton"},{"p":"tarea.tareaChat","l":"User"},{"p":"tarea.tareaChat","l":"UserFrame"},{"p":"tarea.tareaChat","l":"UserPanel"}];updateSearchResults();
